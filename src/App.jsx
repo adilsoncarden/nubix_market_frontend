314130ecb83b3705c14cd413f53b36f5484a9093
Carlos <rcristofer290@gmail.com>
2026-04-22 22:01:09 -0500

    Avance Interfaz de Categoria

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,47 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import MainContent from "./components/MainContent";
 import Login from "./pages/Login";
-import Register from "./pages/Register"; // 1. Importa la nueva página
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
 import "./App.css";
+
+// Creamos un componente interno para poder usar useLocation
+function AppContent() {
+    const location = useLocation();
+    
+    // Definimos en qué ruta queremos ocultar el Navbar y Footer
+    const isDashboard = location.pathname === "/dashboard";
+
+    return (
+        <div className="min-vh-100 d-flex flex-column">
+            {/* Solo muestra el Navbar si NO estamos en dashboard */}
+            {!isDashboard && <Navbar />}
+            
+            <main className="d-flex flex-column flex-grow-1">
+                <div className="flex-grow-1 d-flex flex-column">
+                    <Routes>
+                        <Route path="/" element={<MainContent />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/register" element={<Register />} />
+                        <Route path="/dashboard" element={<Dashboard />} />
+                    </Routes>
+                </div>
+            </main>
+
+            {/* Solo muestra el Footer si NO estamos en dashboard */}
+            {!isDashboard && <Footer />}
+        </div>
+    );
+}
 
 function App() {
     return (
         <Router>
-            <div className="min-vh-100 d-flex flex-column">
-                <Navbar />
-                <main className="d-flex flex-column flex-grow-1">
-                    <div className="flex-grow-1 d-flex flex-column">
-                        <Routes>
-                            <Route path="/" element={<MainContent />} />
-                            <Route path="/login" element={<Login />} />
-                            {/* 2. Agrega la ruta de registro */}
-                            <Route path="/register" element={<Register />} />
-                        </Routes>
-                    </div>
-                </main>
-                <Footer />
-            </div>
+            <AppContent />
         </Router>
     );
 }
